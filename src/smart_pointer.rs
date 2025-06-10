0f3260fba294fdac8cf9e576b6caf953bcc28233
Timothy Julian <timothyace73@gmail.com>
2025-06-10 22:05:44 +0700

    multiple ownership, Rc

--- src/smart_pointer.rs
+++ src/smart_pointer.rs
@@ -1,16 +1,4 @@
-use std::ops::Deref;
-
-fn display_number(value: i32) {
-    println!("{}", value);
-}
-
-fn display_number_reference(value: &i32) {
-    println!("{}", value);
-}
-
-fn print_category(category: &ProductCategory) {
-    println!("{:?}", category);
-}
+use std::{ops::Deref, rc::Rc};
 
 #[derive(Debug)]
 enum ProductCategory {
@@ -28,6 +16,24 @@ impl<T> Deref for MyValue<T> {
     fn deref(&self) -> &Self::Target {
         &self.value
     }
+}
+
+#[derive(Debug)]
+enum Brand {
+    Of(String, Rc<Brand>),
+    End,
+}
+
+fn display_number(value: i32) {
+    println!("{}", value);
+}
+
+fn display_number_reference(value: &i32) {
+    println!("{}", value);
+}
+
+fn print_category(category: &ProductCategory) {
+    println!("{:?}", category);
 }
 
 fn say_hello(name: &String) {
@@ -67,7 +73,9 @@ fn test_dereference() {
 
 #[test]
 fn test_dereference_struct() {
-    let val = MyValue { value: 9 };
+    let val = MyValue {
+        value: 9
+    };
 
     println!("{}", *val);
 }
@@ -75,7 +83,20 @@ fn test_dereference_struct() {
 #[test]
 fn test_dereference_coercion() {
     let name = MyValue {
-        value: "String".to_string(),
+        value: "String".to_string()
     };
     say_hello(&name);
+}
+
+#[test]
+fn test_multiple_ownership() {
+    let apple = Rc::new(Brand::Of("Apple".to_string(), Rc::new(Brand::End)));
+    let laptop = Brand::Of("Laptop".to_string(), Rc::clone(&apple));
+    let smartphone = Brand::Of("Smartphone".to_string(), Rc::clone(&apple));
+
+    println!("{:?}", apple);
+    println!("{:?}", smartphone);
+    println!("{:?}", laptop);
+    println!("Apple reference count: {}", Rc::strong_count(&apple));
+
 }
